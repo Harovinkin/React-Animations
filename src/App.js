a02e1771e216989ed81dcf69a615b0492f988ce3
Harovinkin <kharovinkin@gmail.com>
2020-12-26 15:20:34 +0200

    CSS Transition

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, {useState} from "react";
-import { Transition } from "react-transition-group";
+import { CSSTransition, Transition } from "react-transition-group";
 
 
 function App() {
 const [toggle, setToggle] = useState(true)
+const [toggle2, setToggle2] = useState(true)
 
   return (
     <div className="container">
       <button onClick={() => setToggle(!toggle)}>Toggle</button>
+      <button onClick={() => setToggle2(!toggle2)}>Toggle2</button>
       <hr/>
       <div className={'blocks'}>
       <Transition
@@ -18,9 +20,26 @@ const [toggle, setToggle] = useState(true)
         }}
         mountOnEnter
         unmountOnExit
+        onEnter={() => console.log('onEnter')}
+        onEntering={() => console.log('onEntering')}
+        onEntered={() => console.log('onEntered')}
+        onExit={() => console.log('onExit')}
+        onExiting={() => console.log('onExiting')}
+        onExited={() => console.log('onExited')}
       >
         {state => <div className={`square blue ${state}`}>{state}</div>}
       </Transition>
+      <CSSTransition
+        in={toggle2}
+        timeout={1000}
+        classNames="os"
+        mountOnEnter
+        unmountOnExit
+      >
+        <div className={'square orange'}>
+          {toggle2.toString()}
+        </div>
+      </CSSTransition>
       </div>
     </div>
   );
